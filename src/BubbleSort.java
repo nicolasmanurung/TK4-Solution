6e991059b9381b123be18a2bad7810fddd07f6c9
nicolasmanurung <nicolas.manurung@dana.id>
2023-02-05 00:21:43 +0700

    Add Menu 2 : Add Bubble Sort Ascending

---
+++ src/BubbleSort.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class BubbleSort {
+    public void bubbleSortAscending(ArrayList<Integer> arr) {
+        int n = arr.size();
+        for (int i = 0; i < n - 1; i++) {
+            System.out.printf("Pass %d%n", i + 1);
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.println(arr.stream().map(String::valueOf).collect(Collectors.joining("   ")));
+                if (arr.get(j) > arr.get(j + 1)) {
+                    // swap arr[j+1] and arr[j]
+                    Integer temp = arr.get(j);
+                    arr.set(j, arr.get(j + 1));
+                    arr.set(j + 1, temp);
+                }
+            }
+            System.out.printf("Result of Pass %d%n", i + 1);
+            System.out.println(arr.stream().map(String::valueOf).collect(Collectors.joining("   ")));
+        }
+    }
+
+    public void bubbleSortDescending(ArrayList<Integer> arr) {
+        //TODO("add logic here here")
+    }
+}
